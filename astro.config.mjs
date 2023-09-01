29e13b007a669e1b7a639eab56e43909227037b4
Thilina Gunasekara <thilina.gunasekara@gmail.com>
2023-09-01 19:22:30 +0930

    remove netlify, add vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,22 @@
 import { defineConfig } from "astro/config";
+import vercelServerless from '@astrojs/vercel/serverless';
 
-// https://astro.build/config
 import mdx from "@astrojs/mdx";
-
-// https://astro.build/config
-import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
+
+// https://astro.build/config
 export default defineConfig({
   site: "https://www.thilinag.com",
   integrations: [mdx(), sitemap()],
-  output: "server",
-  adapter: netlify({
-    functionPerRoute: true
- }),
+  output: 'server',
+  adapter: vercelServerless(),
+  vite: {
+    ssr: {
+      noExternal: ['path-to-regexp']
+    }
+  }
 });
