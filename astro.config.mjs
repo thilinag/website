726ba89f29b4afb4a43c2c5e43a3bf6b67bf3835
Thilina Gunasekara <thilina.gunasekara@gmail.com>
2025-02-02 20:04:03 +1030

    update to astro 5 add three, google/model-viewer

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,19 @@
 import { defineConfig } from "astro/config";
-import vercelServerless from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 import sitemap from "@astrojs/sitemap";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.thilinag.com",
-  integrations: [sitemap()],
+  integrations: [sitemap(), mdx()],
   output: 'server',
-  adapter: vercelServerless(),
+  adapter: vercel(),
   vite: {
     ssr: {
       noExternal: ['path-to-regexp']
